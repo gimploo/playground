e53c4df96c28740de7c92bce7f22ce3fb9474d2d
Gokul Krishnan C M <gokulkrishnan949@gmail.com>
2021-04-26 00:51:20 +0530

    Working :D

--- shooter/src/make.bat
+++ shooter/src/make.bat
@@ -6,24 +6,35 @@ set LIBS=C:\libsdl\lib\x64
 set DEPENEDENCIES=SDL2main.lib SDL2.lib shell32.lib
 
 set cwd=%cd%
-set FILES=main.c init.c draw.c input.c
-set FLAGS=/FC /EHsc /std:c11
+set MAINFILE=%cwd%\main.c
+set HEADERS=init.c draw.c input.c 
+set OBJS=%cwd%\init.obj %cwd%\draw.obj %cwd%\input.obj
+set FLAGS=/FC /EHsc
+
+set EXE=..\build\shooter.exe
 
 echo [*] Compiling ...
+echo [*] Creating Objects ...
+
+cl /C %HEADERS% /link /LIBPATH:%LIBS% %DEPENEDENCIES% /SUBSYSTEM:WINDOWS
+
+echo [*] Linking Objects ...
 mkdir ..\build
 pushd ..\build
 
-cl %FLAGS% "%FILE%" /link /LIBPATH:%LIBS% %DEPENEDENCIES% /SUBSYSTEM:WINDOWS /out:shooter.exe
+cl %FLAGS% %MAINFILE% %OBJS% /link /LIBPATH:%LIBS% %DEPENEDENCIES% /SUBSYSTEM:WINDOWS /out:shooter.exe
 
 popd
 
-REM call ..\build\%EXE_NAME%.exe
+del /Q *.obj
+
+call %EXE%
+
 goto EXIT
 
 :CLEAN
 echo [*] Cleaning ...
 del /Q ..\build\*
-rmdir ..\build
 
 :EXIT
 echo [!] DONE! 
